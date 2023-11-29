d8450c5914b4124731acbd00930f981d74b3b94d
asasani <a.sasani@uliege.be>
2023-11-29 09:48:44 +0100

    removed some functions

--- tools.py
+++ tools.py
@@ -17,66 +17,6 @@ def find_index(Mcor, Vec, tol=0.001):
             index = m
     return(index)
 
-def mapping_old(tmp_scll,atoms_ref):
-   tmp_scp = tmp_scll.get_scaled_positions()
-   mscp = atoms_ref.get_scaled_positions()
-   # for i in range(len(tmp_scp)):
-   mstm_lst = []
-   for i in range(len(tmp_scp)):
-      ind = find_index(mscp, tmp_scp[i])
-      if ind != -1:
-            mstm_lst.append(ind)
-      else:
-            raise "Err: Could not map the structures! "
-   return(mstm_lst)
-
-def mapping(str_to_be_map, str_to_map_to,Ret_index=True):
-   # str_to_map_to = self.mySC.get_scaled_positions()
-   natom = len(str_to_map_to.get_scaled_positions())
-   natom2 = len(str_to_be_map.get_scaled_positions())
-   if natom != natom2:
-      raise 'wrong structures'
-   str_cell = np.array(str_to_be_map.get_cell())
-   map_index = np.zeros(natom, dtype=int)
-   xred_maped = np.zeros((natom, 3))
-   for ia, xred_a in enumerate(str_to_map_to.get_scaled_positions()):
-      diff_xred = np.zeros((natom, 3))
-      shift = np.zeros((natom, 3))
-      list_dist = np.zeros(natom)
-      list_absdist = np.zeros((natom, 3))
-      diff_xred = str_to_be_map.get_scaled_positions()-xred_a
-      for ib, b in enumerate(str_to_be_map.get_scaled_positions()):
-            if diff_xred[ib, 0] > 0.5:
-               diff_xred[ib, 0] = 1 - diff_xred[ib, 0]
-               shift[ib, 0] = -1
-            if diff_xred[ib, 1] > 0.5:
-               diff_xred[ib, 1] = 1 - diff_xred[ib, 1]
-               shift[ib, 1] = -1
-            if diff_xred[ib, 2] > 0.5:
-               diff_xred[ib, 2] = 1 - diff_xred[ib, 2]
-               shift[ib, 2] = -1
-            if diff_xred[ib, 0] < -0.5:
-               diff_xred[ib, 0] = -1-diff_xred[ib, 0]
-               shift[ib, 0] = 1
-            if diff_xred[ib, 1] < -0.5:
-               diff_xred[ib, 1] = -1-diff_xred[ib, 1]
-               shift[ib, 1] = 1
-            if diff_xred[ib, 2] < -0.5:
-               diff_xred[ib, 2] = -1-diff_xred[ib, 2]
-               shift[ib, 2] = 1
-            list_absdist[ib, :] = np.dot(str_cell, diff_xred[ib, :])
-            list_dist[ib] = np.sqrt(
-         np.dot(list_absdist[ib, :], np.transpose(list_absdist[ib, :])))
-      map_index[ia] = np.where(list_dist == min(list_dist))[0][0]
-      xred_maped[ia, :] = str_to_be_map.get_scaled_positions(
-      )[map_index[ia], :] + shift[map_index[ia], :]
-
-   if Ret_index:
-      return(map_index)
-   maped_str = Atoms(numbers=str_to_map_to.get_atomic_numbers(
-   ), scaled_positions=xred_maped, cell=str_to_be_map.get_cell())
-   return(maped_str)
-
 def map_ordered_strctures(str_1,str_2,tol=0.5):  # the ordering is correct
     red_1 = str_1.get_scaled_positions()
     red_2 = str_2.get_scaled_positions()
